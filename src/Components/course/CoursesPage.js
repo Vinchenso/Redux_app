c2e59b7ac05f47199eaaf0190fc99d32b64b2edf
Vinchenso <chensok@gmail.com>
2016-12-12 18:17:57 +0200

    Foundation and Components and Router Completed

---
+++ src/Components/course/CoursesPage.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from 'react';
+
+const CoursesPage = props => {
+    return (
+        <div>
+          <h1> Courses </h1>
+        </div>
+    );
+};
+
+CoursesPage.propTypes = {
+    
+};
+
+export default CoursesPage;
