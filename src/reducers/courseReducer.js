988a102878445b9a2eb4673c57213a5a692c45fa
Vinchenso <chensok@gmail.com>
2017-01-09 12:20:05 +0200

    AsyncRedux: Dispatch Action on PageLoad

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -6,7 +6,8 @@ import * as types from '../actions/actionTypes';
        return [...state,
          Object.assign({},action.course)
        ];
-
+     case types.LOAD_COURSES_SUCCESS:
+       return action.courses; 
      default:
       return state;
    }
