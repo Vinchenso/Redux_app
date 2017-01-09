717effd449586b04d405c40ce93f0179f533646d
Vinchenso <chensok@gmail.com>
2017-01-09 11:59:22 +0200

    adding thunks to store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
-//import reduxImmutatableStateInvariant from 'redux-immutable-state-invariant';
+import reduxImmutatableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
-    initialState//,
-    //applyMiddleware(reduxImmutatableStateInvariant())
+    initialState,
+    applyMiddleware(thunk, reduxImmutatableStateInvariant())
   );
 }
