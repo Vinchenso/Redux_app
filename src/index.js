2c64ea4e9c2d3fdce31bd8a9f48f6e5af9636c19
Vinchenso <chensok@gmail.com>
2017-01-09 12:14:15 +0200

    AsyncRedux: Creating load course thunk

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-import routes from './Components/routes';
+import routes from './components/routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configureStore';
